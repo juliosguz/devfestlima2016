c5d35c9b58f12242b1344d86693f50b9ce791196
Julio Silva <julio.sguz@gmail.com>
2016-09-04 12:17:29 -0400

    Cambios menores - Logo 2016 DevFest season - Titulo en logos de comunidad - Información en pie de pagina

--- app/metadata/footer.js
+++ app/metadata/footer.js
@@ -8,6 +8,16 @@ module.exports = {
             text: 'GDG Open',
             url: 'http://gdgopen.com/',
             newTab: true
+          },
+          {
+            text: 'Google Developers',
+            url: 'https://developers.google.com/',
+            newTab: true
+          },
+          {
+            text: 'Google Codelabs',
+            url: 'https://codelabs.developers.google.com/',
+            newTab: true
           }
         ]
       },
